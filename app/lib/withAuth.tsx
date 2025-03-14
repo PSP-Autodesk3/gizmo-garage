b0da46387078eb58565312cc9c27af0a37d12d9a
Robert Jenner <85253281+WebExtension1@users.noreply.github.com>
2025-03-14 17:44:26 +0000

    Updated variables

--- app/lib/withAuth.tsx
+++ app/lib/withAuth.tsx
@@ -37,7 +37,7 @@ export default function withAuth<T extends object>(
             router.push("/");
           } else if (user && !sessionToken) {
             // If the user is signed in, not autodesk authenticated, but the database doesn't exist, add admin-settings to the protected page
-            const response = await fetch(`http://${process.env.SERVER_HOST}:3001/database/exists`);
+            const response = await fetch(`http://${process.env.NEXT_PUBLIC_SERVER_HOST}:3001/database/exists`);
             const exists = await response.json();
             if (exists[0]?.DatabaseExists === 0 && pathname !== "/admin-settings") {
               router.push("/");
