cc26e67c16db77fa8cd85a7372ff372a3f0352a4
Felix <c3032522@hallam.shu.ac.uk>
2025-03-20 16:51:57 +0000

    Checkbox styling, Files in tree styling and File svgs added.

--- app/project/[...slug]/page.tsx
+++ app/project/[...slug]/page.tsx
@@ -144,9 +144,13 @@ function Home({ params }: ParamProps) {
           let foldersObjects = objects.filter((object: File) => object.folder_id === folder.folder_id );
           
           foldersObjects.forEach((object: File) => {
-            const file = document.createElement("p");
-            file.innerHTML = object.name;
-            file.className = `ml-[30px] hidden tree-file`;
+            const file = document.createElement("button");
+            file.className = "pl-4 flex items-center gap-2 py-2 px-3 text-slate-300 hover:text-slate-100 transition-colors duration-200 flex-1 text-left hidden tree-file";
+            file.innerHTML = `
+              <svg class="ml-2 w-5 h-5 text-slate-400 flex-shrink-0" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+              </svg>
+              ${object.name}`;
             details.appendChild(file);
           });
 
@@ -313,10 +317,11 @@ function Home({ params }: ParamProps) {
                 </svg>
                 {project.replace(/%2B/g, ' ')}
               </button>
-              <div className="flex flex-row ml-[30px] mt-[20px] gap-[15px]">
+              <div className="flex flex-row mt-[20px] gap-[7.5px]">
                 <input
                   id="file-checkbox"
                   type="checkbox"
+                  className="m-1 w-4 h-4 bg-red-600 checked:bg-green-600 border-red-600 checked:border-green-600 border-2 appearance-none rounded transition-colors duration-200"
                 />
                 <p>Show files</p>
               </div>
@@ -363,7 +368,7 @@ function Home({ params }: ParamProps) {
 
               {/* Files */}
               <div id="files" className="mx-8 my-4">
-                <h1 className="my-4 text-3xl">Files</h1>
+                <h1 className="my-4 text-3xl">Files:</h1>
                 <FileList
                   files={filteredFiles}
                 />
--- app/shared/components/fileList.tsx
+++ app/shared/components/fileList.tsx
@@ -11,21 +11,20 @@ export default function FileList({ files }: { files: File[] }) {
     return (
         <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-4">
             {!files ? (
-                <>
-                    <div>
-                        <SkeletonTheme baseColor='#0f172a' highlightColor='#1e293b' enableAnimation duration={0.5}>
-                            <Skeleton width={100} height={100} style={{ margin: '5px' }} />
-                        </SkeletonTheme>
-                    </div>
-                </>
+                <SkeletonTheme baseColor='#0f172a' highlightColor='#1e293b' enableAnimation duration={0.5}>
+                    <Skeleton width={100} height={100} style={{ margin: '5px' }} />
+                </SkeletonTheme>
             ) : (
                 Array.isArray(files) && files.length > 0 && (
                     files.map((file) => (
                         <div key={file.object_id}>
                             <button
-                                className="bg-slate-900 rounded-lg text-xl my-4 px-4 py-2"
+                                className="bg-slate-900 w-full rounded-lg text-xl my-4 px-4 py-2 gap-2 items-center flex"
                                 onClick={() => { }}
                             >
+                                <svg className="w-5 h-5 text-slate-400 flex-shrink-0" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                                </svg>
                                 {file.name}
                             </button>
                             {Array.isArray(file.tags) && file.tags.length > 0 && (
