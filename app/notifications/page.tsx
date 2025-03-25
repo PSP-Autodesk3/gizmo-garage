2245cf53402ece6d165ea25e98d4c16dbf9e3371
Felix <c3032522@hallam.shu.ac.uk>
2025-03-25 20:07:29 +0000

    Noti Page and Nav Styled.

--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -64,27 +64,27 @@ function Home() {
     return (
         <>
             <BackBtnBar />
-            <h1 className="text-3xl font-bold text-slate-200 text-center mt-8">
-                Notifications
+            <h1 className="text-3xl font-bold text-slate-900 dark:text-slate-200 text-center mt-8">
+                Your Invites
             </h1>
 
-            <div className='bg-slate-900 p-4 mx-8 my-4 rounded-lg'>
+            <div className='bg-indigo-200/50 dark:bg-slate-900 border border-slate-700/50 text-slate-900 dark:text-slate-200 p-4 mx-8 my-4 rounded-lg'>
                 {Array.isArray(invites) && invites.length > 0 ? (
                     invites.map((invite) => (
                         <div className='flex justify-between items-center mb-4' key={invite.project + invite.author}>
                             <div>
-                                <p className="text-4xl pb-5 font-bold">{invite.project}</p>
-                                <p>Author: <span className='rounded-full m-2 p-2 bg-blue-600'>{invite.author}</span></p>
+                                <p className="text-4xl pb-5 font-semibold">{invite.project}</p>
+                                <p className='font-semibold'>Author: {invite.author}</p>
                             </div>
                             <div className='content-center flex'>
                                 <button
-                                    className="px-6 bg-green-700 py-3 mr-4 text-lg font-medium rounded-lg transition-all duration-300 hover:bg-green-500 hover:scale-105 shadow-lg hover:shadow-green-500/50"
+                                    className="text-white px-6 bg-green-700 py-3 mr-4 text-lg font-medium rounded-lg transition-all duration-300 hover:bg-green-500 hover:scale-105 shadow-lg hover:shadow-green-500/50"
                                     onClick={() => acceptInvite(invite)}
                                 >
                                     Accept
                                 </button>
                                 <button
-                                    className="px-6 py-3 bg-red-700 mr-4 text-lg font-medium rounded-lg transition-all duration-300 hover:bg-red-500 hover:scale-105 shadow-lg hover:shadow-red-500/50"
+                                    className="text-white px-6 py-3 bg-red-700 mr-4 text-lg font-medium rounded-lg transition-all duration-300 hover:bg-red-500 hover:scale-105 shadow-lg hover:shadow-red-500/50"
                                     onClick={() => declineInvite(invite)}
                                 >
                                     Decline
