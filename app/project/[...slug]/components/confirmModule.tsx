dccbfcb70a66eb7993075a48ced16dea93e7a8b2
Robert Jenner <85253281+WebExtension1@users.noreply.github.com>
2025-03-27 16:03:05 +0000

    Hotfix: File uploads with page refresh

--- app/project/[...slug]/components/confirmModule.tsx
+++ app/project/[...slug]/components/confirmModule.tsx
@@ -356,7 +356,6 @@ export default function ConfirmModule({ itemType, projectID, type, id, setConfir
                     <button
                         className={`px-6 m-1 py-3 text-lg font-medium bg-indigo-600 rounded-lg transition-all duration-300 hover:bg-indigo-500 hover:scale-105 shadow-lg hover:shadow-indigo-500/50 text-white` + (itemName.trim() ? "" : " opacity-50 cursor-not-allowed")}
                         disabled={!itemName.trim()}
-                        onClick={() => sessionStorage.setItem("reload", "yes")}
                     >
                         Create
                     </button>
